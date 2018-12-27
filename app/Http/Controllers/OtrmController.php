c3522efda88daa5a28b3fb18cf973adbc84fc5bc
Pablo Salgado <ps@salgado.cl>
2018-12-27 19:04:18 -0300

    Update OtrmController.php

--- app/Http/Controllers/OtrmController.php
+++ app/Http/Controllers/OtrmController.php
@@ -11,7 +11,14 @@ class OtrmController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    public function index(Request $request)
     {
+      /*if($request->ajax())
+      {
+        return response()->json([
+            ['id' => 1, 'name' =]
+        ]);
+      }*/
         return view('otrm.index');
     }
 
