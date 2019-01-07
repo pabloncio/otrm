b1c96298d9857161a0a4f5b7edd9bfc14242d7a0
Pablo Salgado <ps@salgado.cl>
2019-01-07 18:32:36 -0300

    repair vue and views with Cors

--- resources/assets/js/components/Otrm/List.vue
+++ resources/assets/js/components/Otrm/List.vue
@@ -1,28 +1,37 @@
 <template>
-      <div class="row">
-        <table class="table">
-          <thead>
-            <tr>
-              <th scope="col">#</th>
-              <th scope="col">Date In</th>
-              <th scope="col">Date Out</th>
-              <th scope="col">Amount</th>
-              <th scope="col">Payment</th>
-              <th scope="col">Option</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr v-for="otrm in otrms">
-              <th scope="row">1</th>
-              <td>{{ otrm.date_in }}</td>
-              <td>{{ otrm.date_out }}</td>
-              <td>{{ otrm.amount }}</td>
-              <td>{{ otrm.payment }}</td>
-              <td><a href="#" class="btn btn-default">Ver mas...</a></td>
-            </tr>
-          </tbody>
-        </table>
+  <div classs="container">
+    <nav class="sidebar">
+      <div class="row col-md-4">
+
       </div>
+
+    </nav>
+    <div class="row col-md-10">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Date In</th>
+            <th scope="col">Date Out</th>
+            <th scope="col">Amount</th>
+            <th scope="col">Payment</th>
+            <th scope="col">Option</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="otrms in otrm">
+            <th scope="row">{{ otrm.id }}</th>
+            <td>{{ otrm.date_in }}</td>
+            <td>{{ otrm.date_out }}</td>
+            <td>{{ otrm.amount }}</td>
+            <td>{{ otrm.payment }}</td>
+            <td><a href="#" class="btn btn-default">Ver mas...</a></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+
 
 </template>
 
@@ -30,12 +39,17 @@
     export default {
         data() {
             return {
-
+              otrms:[
+                  {id => 1, date_in =>'27-12-2018', date_out=>'30-12-2018', amount=>20000, payment=>3000, status=>'pending'},
+                  {id => 2, date_in =>'27-12-2018', date_out=>'30-12-2018', amount=>20000, payment=>3000, status=>'pending'},
+                  {id => 3, date_in =>'27-12-2018', date_out=>'30-12-2018', amount=>20000, payment=>3000, status=>'pending'}
+              ]
             }
 
         },
         mounted() {
-            axios.get('http://127.0.0.1:8000/otrm').then(response => (this.otrms = response.data))
+          //axios.get('http://127.0.0.1:8000/otrm').then(response => (this.otrms = response.data))
+
         }
     }
 </script>
