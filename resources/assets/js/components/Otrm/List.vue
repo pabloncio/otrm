da379b9f0ce4f5156c05973d3d9442ba0629023b
Pablo Salgado <ps@salgado.cl>
2019-01-07 19:04:44 -0300

    Rare Code with Vue Problem persist

--- resources/assets/js/components/Otrm/List.vue
+++ resources/assets/js/components/Otrm/List.vue
@@ -20,11 +20,11 @@
         </thead>
         <tbody>
           <tr v-for="otrms in otrm">
-            <th scope="row">{{ otrm.id }}</th>
-            <td>{{ otrm.date_in }}</td>
-            <td>{{ otrm.date_out }}</td>
-            <td>{{ otrm.amount }}</td>
-            <td>{{ otrm.payment }}</td>
+            <th scope="row"> {{ otrm.id }} </th>
+            <td> @{{ otrm.date_in }} </td>
+            <td> @{{ otrm.date_out }} </td>
+            <td> @{{ otrm.amount }} </td>
+            <td> @{{ otrm.payment }} </td>
             <td><a href="#" class="btn btn-default">Ver mas...</a></td>
           </tr>
         </tbody>
@@ -39,16 +39,16 @@
     export default {
         data() {
             return {
-              otrms:[
-                  {id => 1, date_in =>'27-12-2018', date_out=>'30-12-2018', amount=>20000, payment=>3000, status=>'pending'},
-                  {id => 2, date_in =>'27-12-2018', date_out=>'30-12-2018', amount=>20000, payment=>3000, status=>'pending'},
-                  {id => 3, date_in =>'27-12-2018', date_out=>'30-12-2018', amount=>20000, payment=>3000, status=>'pending'}
+              otrms: [
+                  {id: 1, date_in:'27-12-2018', date_out:'30-12-2018', amount:20000, payment:3000, status:'pending'},
+                  {id: 2, date_in:'27-12-2018', date_out:'30-12-2018', amount:20000, payment:3000, status:'pending'},
+                  {id: 3, date_in:'27-12-2018', date_out:'30-12-2018', amount:20000, payment:3000, status:'pending'}
               ]
             }
 
         },
         mounted() {
-          //axios.get('http://127.0.0.1:8000/otrm').then(response => (this.otrms = response.data))
+          //axios.get('http://127.0.0.1:8000/otrm').then(response: (this.otrms = response.data))
 
         }
     }
